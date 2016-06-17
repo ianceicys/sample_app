c566b12fb08296daad985ee2a69dffd864a963ec
EC2 Default User <ec2-user@ip-10-50-50-150.ec2.internal>
2016-06-17 18:27:15 +0000

    Modified test script

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,7 +18,7 @@ describe UsersController do
       before(:each) do
         @user = test_sign_in(Factory(:user))
         second = Factory(:user, :name => "Bob", :email => "another@example.com")
-        third  = Factory(:user, :name => "Ben", :email => "another@example.net")
+        third  = Factory(:user, :name => "Ian", :email => "another@example.net")
         
         30.times do
           Factory(:user, :name => Factory.next(:name),
